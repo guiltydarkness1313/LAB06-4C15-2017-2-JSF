83b21de4dd08e9daca477be22205375d33470607
Paul Frank <paul.frankpc@gmail.com>
2017-09-20 11:49:14 -0500

    first commit

---
+++ src/main/java/com/tecsup/lab6/jsf/bean/NavigationCtrl.java
@@ -0,0 +1,38 @@
+package com.tecsup.lab6.jsf.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="navigationCtrl",eager=true)
+@RequestScoped
+public class NavigationCtrl {
+
+	@ManagedProperty(value="#{param.pageId}")
+	private String pageId;
+	
+	public String getPageId() {
+		return pageId;
+	}
+
+	public void setPageId(String pageId) {
+		this.pageId = pageId;
+	}
+
+	public String goToPage1(){
+		return "page 1";
+	}
+	
+	public String showPage(){
+		if(pageId==null) {
+			return "page2";
+		}
+		if(pageId.equals("1")) {
+			return "page1";
+		}else if(pageId.equals("2")) {
+			return "page2";
+		}else {
+			return "page2";
+		}
+	}
+}
---
+++ src/main/java/com/tecsup/lab6/jsf/bean/NavigationCtrl2.java
@@ -0,0 +1,25 @@
+package com.tecsup.lab6.jsf.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "navigationCtrl2", eager = true)
+@RequestScoped
+public class NavigationCtrl2 implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String processPage1 () {
+		return "page";
+	}
+
+	public String processPage2 () {
+		return "page";
+	}
+
+}
